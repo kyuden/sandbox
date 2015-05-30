1460f4b6774a17b187cc058e37c2fbfe2abdf985
Kyuden <msmsms.um@gmail.com>
2015-05-30 23:10:10 +0900

    patern match

--- erlang/useless.erl
+++ erlang/useless.erl
@@ -1,5 +1,8 @@
 -module(useless).
--export([add/2, hello/0, greet_and_add_two/1]).
+-export([add/2,
+         hello/0,
+         greet_and_add_two/1,
+         greet/2]).
 
 add(A,B) ->
   A + B.
@@ -13,3 +16,9 @@ greet_and_add_two(X) ->
   hello(),
   add(X,2).
 
+greet(male, Name) ->
+  io:format("Hello, Mr. ~s", [Name]);
+greet(female, Name) ->
+  io:format("Hello, Mrs. ~s", [Name]);
+greet(_, Name) ->
+  io:format("Hello, ~s", [Name]).
