df05e47678fce2f7ad51ec1062cfbcc811a9d160
Kyuden <msmsms.um@gmail.com>
2015-05-30 21:54:13 +0900

    Hello Erlang

---
+++ erlang/useless.erl
@@ -0,0 +1,15 @@
+-module(useless).
+-export([add/2, hello/0, greet_and_add_two/1]).
+
+add(A,B) ->
+  A + B.
+
+%% Shows gretings
+%% io:format/1 is the standard function to output text.
+hello() ->
+  io:format("Hello World!~n").
+
+greet_and_add_two(X) ->
+  hello(),
+  add(X,2).
+
