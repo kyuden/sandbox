328a0d3f6479bb76bdd000ae7d9bbb89cdb1c156
Kyuden <msmsms.um@gmail.com>
2015-06-07 15:18:19 +0900

    Use link

---
+++ erlang/linkmon.erl
@@ -0,0 +1,20 @@
+-module(linkmon).
+-compile(export_all).
+
+myproc() ->
+  timer:sleep(5000),
+  exit(reson).
+
+chain(0) ->
+  receive
+    _ -> ok
+  after 2000 ->
+    exit("chan dies here")
+  end;
+chain(N) ->
+  Pid = spawn(fun() -> chain(N-1) end),
+  link(Pid),
+  receive
+    _ -> ok
+  end.
+
