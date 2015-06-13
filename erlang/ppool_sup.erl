12f4feb93c54619b920239b1c69ee7da336b4936
Kyuden <msmsms.um@gmail.com>
2015-06-14 02:07:43 +0900

    Create ppool_sup

---
+++ erlang/ppool_sup.erl
@@ -0,0 +1,18 @@
+-module(ppool_sup).
+-behavior(supervisor).
+-compile(export_all).
+
+start_link(Name, Limit, MFA) ->
+  supervisor:start_link(?MODULE, {Name, Limit, MFA}).
+
+init({Name, Limit, MFA}) ->
+  MaxRestart = 1,
+  MaxTime = 3600,
+  {ok, {{one_for_all, MaxRestart, MaxTime},
+        [{serv,
+          {ppool_serv, start_link, [Name, Limit, self(), MFA]},
+          permanent,
+          5000,
+          worker,
+          [ppool_serv]}]}}.
+
