99d5af80f3ecef75ff27ed4b7cbfb39432b3453a
Kyuden <msmsms.um@gmail.com>
2015-06-16 01:28:52 +0900

    Refactor server

--- erlang/kitty/kitty_server.erl
+++ erlang/kitty/kitty_server.erl
@@ -4,59 +4,33 @@
 -record(cat, {name, color=green, description}).
 
 start_link() ->
-  spawn_link(fun init/0).
+  my_server:start_link(?MODULE, []).
 
-init() -> loop([]).
+init([]) -> [].
 
 order_cat(Pid, Name, Color, Description) ->
-  Ref = erlang:monitor(process, Pid),
-  Pid ! {self(), Ref, {order, Name, Color, Description}},
-  receive
-    {Ref, Cat} ->
-      erlang:demonitor(Ref, [flush]),
-      Cat;
-    {'DOWN', Ref, process, Pid, Reason} ->
-      erlang:error(Reason)
-  after 5000 ->
-    erlang:error(timeout)
-  end.
-
-return_cat(Pid, Cat=#cat{}) ->
-  Pid ! {return, Cat},
-  ok.
+  my_server:call(Pid, {order, Name, Color, Description}).
 
 close_shop(Pid) ->
-  Ref = erlang:monitor(process, Pid),
-  Pid ! {self(), Ref, terminate},
-  receive
-    {Ref, ok} ->
-      erlang:demonitor(Ref, [flush]),
-      ok;
-    {'DOWN', Ref, process, Pid, Reason} ->
-      erlang:error(Reason)
-  after 5000 ->
-    erlang:error(timeout)
-  end.
+  my_server:call(Pid, terminate).
 
-loop(Cats) ->
-  receive
-    {Pid, Ref, {order, Name, Color, Description}} ->
-      if Cats =:= [] ->
-           Pid ! {Ref, make_cat(Name, Color, Description)},
-           loop(Cats);
-         Cats =/= [] ->
-           Pid ! {Ref, hd(Cats)},
-           loop(tl(Cats))
-      end;
-    {return, Cat=#cat{}} ->
-      loop([Cat|Cats]);
-    {Pid, Ref, terminate} ->
-      Pid ! {Ref, ok},
-      terminate(Cats);
-    Unknown ->
-      io:format("Unknown message: ~p~n", [Unknown]),
-      loop(Cats)
-  end.
+return_cat(Pid, Cat=#cat{}) ->
+  my_server:cast(Pid, {return, Cat}).
+
+handle_call({order, Name, Color, Description}, From, Cats) ->
+  if Cats =:= [] ->
+       my_server:reply(From, make_cat(Name, Color, Description)),
+       Cats;
+     Cats =/= [] ->
+       my_server:reply(From, hd(Cats)),
+       tl(Cats)
+  end;
+handle_call(terminate, From, Cats) ->
+  my_server:reply(From, ok),
+  terminate(Cats).
+
+handle_cast({return, Cat=#cat{}}, Cats) ->
+  [Cat|Cats].
 
 make_cat(Name, Col, Desc) ->
   #cat{name=Name, color=Col, description=Desc}.
--- erlang/kitty/my_server.erl
+++ erlang/kitty/my_server.erl
@@ -3,7 +3,7 @@
 
 call(Pid, Msg) ->
   Ref = erlang:monitor(process, Pid),
-  Pid ! {self(), Ref, Msg},
+  Pid ! {sync, self(), Ref, Msg},
   receive
     {Ref, Reply} ->
       erlang:demonitor(Ref, [flush]),
@@ -13,4 +13,29 @@ call(Pid, Msg) ->
   after 5000 ->
     erlang:error(timeout)
   end.
+
+cast(Pid, Msg) ->
+  Pid ! {async, Msg},
+  ok.
+
+reply({Pid, Ref}, Reply) ->
+  Pid ! {Ref, Reply}.
+
+start(Module, InitialState) ->
+  spawn(fun() -> init(Module, InitialState) end).
+
+start_link(Module, InitialState) ->
+  spawn_link(fun() -> init(Module, InitialState) end).
+
+init(Module, InitialState) ->
+  loop(Module, Module:init(InitialState)).
+
+loop(Module, State) ->
+  receive
+    {async, Msg} ->
+      loop(Module, Module:handle_cast(Msg, State));
+    {sync, Pid, Ref, Msg} ->
+      loop(Module, Module:handle_call(Msg, {Pid, Ref}, State))
+  end.
+
 
